2c42362c4470f8231e8b9a600640ea7efee214f5
VitProg <vitprog@gmail.com>
2015-11-09 01:55:07 +0300

    update

--- AngularTimeagoAsset.php
+++ AngularTimeagoAsset.php
@@ -12,7 +12,7 @@ class AngularTimeagoAsset extends \yii\web\AssetBundle
     /**
      * @inheritdoc
      */
-    public $sourcePath = '@bower/angular-timeago';
+    public $sourcePath = '@bower/angular-timeago/src';
 
     /**
      * @inheritdoc
